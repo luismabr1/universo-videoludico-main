1dd755f877f734f1afc7184d49f3dbe7f2ae3063
Luis Manuel Brito <lmbritor@pm.com>
2024-10-25 15:25:56 -0400

    update eslint again

--- src/components/ThemeScript.js
+++ src/components/ThemeScript.js
@@ -1,18 +1,16 @@
-'use client'
+"use client";
 
 import Script from "next/script";
 
 const ThemeScript = () => {
   return (
-    <Script strategy="beforeInteractive">
-    {
-      `document.documentElement.classList.toggle(
+    <Script id="script" strategy="beforeInteractive">
+      {`document.documentElement.classList.toggle(
 'dark',
 localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-)`
-    }
-  </Script>
-  )
-}
+)`}
+    </Script>
+  );
+};
 
-export default ThemeScript
+export default ThemeScript;
