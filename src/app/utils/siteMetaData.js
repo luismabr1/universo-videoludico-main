ad65c957e9af430cd35be111c169d6e034e802da
Luis Manuel Brito <lmbritor@pm.com>
2024-11-01 16:24:31 -0400

    change metadata

--- src/app/utils/siteMetaData.js
+++ src/app/utils/siteMetaData.js
@@ -1,20 +1,16 @@
 const siteMetadata = {
-    title: 'Universo de posibilidades',
-    author: 'LuismaBr',
-    headerTitle: 'U&niverso Videoludico',
+    title: 'Universo de Posibilidades',
+    author: 'LuismaBR',
+    headerTitle: 'Universo Videoludico',
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://www.universovideoludico.com/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
+    email: 'contacto@universovideoludico.com', 
+    github: 'https://github.com/luismabr',
+    linkedin: 'https://www.linkedin.com/in/luismabr/',
     locale: 'en-US',
   }
   
